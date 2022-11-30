0486c96a8f25398817f4ec214410c695f7560e07
Anton Andriienko <toxa1818@icloud.com>
2022-11-30 14:30:47 +0200

    models added and add app services to INSTALLED_APPS

--- services/models.py
+++ services/models.py
@@ -1,4 +1,41 @@
 from django.db import models
 
 
+class Client(models.Model):
+    first_name = models.CharField(max_length=20)
+    last_name = models.CharField(max_length=20)
+    phone_number = models.CharField(max_length=20)
+    car = models.CharField(max_length=30)
 
+    def __str__(self):
+        return [self.first_name,
+                self.last_name,
+                self.phone_number,
+                self.car]
+
+
+class Worker(models.Model):
+    first_name = models.CharField(max_length=20)
+    last_name = models.CharField(max_length=20)
+    experience = models.IntegerField()
+
+    def __str__(self):
+        return [self.first_name,
+                self.last_name,
+                self.experience]
+
+
+class Service(models.Model):
+    name = models.CharField(max_length=50)
+    time = models.IntegerField()
+    cost = models.IntegerField()
+
+    def __str__(self):
+        return [self.name, self.time, self.cost]
+
+
+class Timetable(models.Model):
+    start_time = models.DateTimeField(auto_now_add=False)
+    client = models.OneToOneField(Client, on_delete=models.SET_NULL)
+    list_of_services = models.ManyToManyField(Service)
+    worker = models.ForeignKey(Worker, on_delete=models.CASCADE)
